acbe5d552897f4dfc5bf1ca16b91369694477524
Palmer Harrell <palmerharrell@me.com>
2016-02-29 15:41:26 -0600

    Jasmine setup

---
+++ spec/gauntletSpec.js
@@ -0,0 +1,13 @@
+describe("The specification for Gauntlet", function() {
+
+  it("should have a Gauntlet.Combatants.Player function", function() {
+    expect(Gauntlet.Combatants.Player).toBeDefined();
+  });
+
+  it("should have a Gauntlet function", function() {
+    expect(Gauntlet).toBeDefined();
+  });
+
+
+
+ });
